ce452ea377498250fa341536f9f4d7cb3f3fcb25
kuchalakanti <kuchalakanti@gmail.com>
2024-11-10 07:02:11 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = "sadsfsadfadsfadsasfs"
-  secret_key = "sdasdfassadfasfasfsddd"
+  access_key = "AKIA2MNVMDG5YOMJQKXH"
+  secret_key = "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"
   region = "us-east-1"
 
 
